57e92dde4dc190638a2f77393980ae8956314769
Spatchy <17989046+Spatchy@users.noreply.github.com>
2024-08-28 22:21:11 +0100

    Create a skeleton GUI for main app view (#2)
    Co-authored-by: Spatchy <spatchy@spatchy.net>

--- src/pages/Toplevel.tsx
+++ src/pages/Toplevel.tsx
@@ -1,4 +1,17 @@
 import { createRoot } from 'react-dom/client';
+import Sidebar from "./sidebar/Sidebar"
+import ContactGrid from './contacts/ContactGrid';
+
+const testContactsArray = [...Array(26)].flatMap((_, i) => [String.fromCharCode(97 + i), String.fromCharCode(65 + i)]);
+
+const Toplevel = () => {
+  return (
+    <div className="flex w-screen h-screen">
+      <Sidebar />
+      <ContactGrid contacts={testContactsArray}/>
+    </div>
+  )
+}
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<Toplevel />);
