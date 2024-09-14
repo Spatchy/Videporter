215293338ada9577c51447046831ae2c678071e3
Spatchy <spatchy@spatchy.net>
2024-09-14 18:05:13 +0100

    Add buttons to sidebar

--- src/pages/sidebar/Sidebar.tsx
+++ src/pages/sidebar/Sidebar.tsx
@@ -1,6 +1,21 @@
+import { Person } from '@mui/icons-material';
+import { Groups } from '@mui/icons-material';
+import { Settings } from '@mui/icons-material';
+
 const Sidebar = () => {
   return (
-    <div className="w-16 h-full bg-red-600"></div>
+    <div className="w-16 h-full border-r-2 border-r-zinc-400 flex flex-col">
+      <button>
+        <Person />
+      </button>
+      <button>
+        <Groups />
+      </button>
+      <div className="flex-1"></div>
+      <button>
+        <Settings />
+      </button>
+    </div>
   )
 }
 
